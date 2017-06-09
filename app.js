fd5d971237bfc8b0d4ee499a33b5e49b97a8e3e2
Firefly4266 <johnathon@Icon-Base.(none)>
2017-06-08 23:25:51 -0700

    Fixed a sentence

--- app.js
+++ app.js
@@ -1,67 +1,84 @@
 'use strict';
 
-var userName = prompt('Welcome to my page.  If you ready to play introduce yourself.');
-alert('Nice to meet you ' + userName + '. I am going to ask you some questions about me.  No pressure really?');
-console.log('The user name is ' + userName);
+var userName;
+var response;
+
+function user() {
+  userName = prompt('Welcome to my page.  If you ready to play introduce yourself.');
+  alert('Nice to meet you ' + userName + '. I am going to ask you some questions about me.  No pressure really? A short series of Yes or No questions.  Please answer Yes (y) or NO (n).');
+  console.log('The user name is ' + userName);
+}
+user(userName);
 
 var gameCount = 0;
 var answer = 'yes';
 var wrong = 'no';
 
-var response1 = prompt('Was I a successful Bridge Officer?').toLowerCase();
-if (response1 === answer || response1 === answer[0]) {
-  alert('You are correct ' + userName + ', I\'d been at it for a while.');
-  gameCount++;
-}else if (response1 === wrong || response1 === wrong[0]) {
-  alert('You are so wrong! ' + userName + ', didn\'t you do your homework?');
-}else{
-  alert(userName + ', that was not the answer I was expecting...');
+function question1() {
+  response = prompt('Was I a successful Bridge Officer?').toLowerCase();
+  if (response === answer || response === answer[0]) {
+    alert('You are correct ' + userName + ', I\'d been at it for a while.');
+    gameCount++;
+  }else if (response === wrong || response === wrong[0]) {
+    alert('You are so wrong! ' + userName + ', didn\'t you do your homework?');
+  }else{
+    alert(userName + ', that was not the answer I was expecting...');
+  }
+  console.log('The response from ' + userName + ' was ' + response);
 }
-console.log('The response from ' + userName + ' was ' + response1);
 
-var response2 = prompt('Do I miss being at sea?').toLowerCase();
-if (response2 === answer || response2 === answer[0]) {
-  alert('You are beyond wrong ' + userName + '! I\'m really happy about not having to leave home for weeks at a time!');
-}else if (response2 === wrong || response2 === wrong[0]) {
-  alert('That\'s Soooo true ' + userName + '!!! I\'ll never work at sea again if I can help it!');
-  gameCount++;
-}else{
-  alert(userName + ', that was not the answer I was expecting...');
+function question2() {
+  response = prompt('Do I miss being at sea?').toLowerCase();
+  if (response === answer || response === answer[0]) {
+    alert('You are beyond wrong ' + userName + '! I\'m really happy about not having to leave home for weeks at a time!');
+  }else if (response === wrong || response === wrong[0]) {
+    alert('That\'s Soooo true ' + userName + '!!! I\'ll never work at sea again if I can help it!');
+    gameCount++;
+  }else{
+    alert(userName + ', that was not the answer I was expecting...');
+  }
+  console.log('The response from ' + userName + ' was ' + response);
 }
-console.log('The response from ' + userName + ' was ' + response2);
 
-var response3 = prompt('Am I just bored doing 201 again?').toLowerCase();
-if (response3 === answer || response3 === answer[0]) {
-  alert('Wrong answer ' + userName + ', I pick something up every time I go through a Code Fellows class.  I love that!');
-}else if (response3 === wrong || response3 === wrong[0]) {
-  alert('You are correct ' + userName + ', I\'m having fun learning with my cohort.');
-  gameCount++;
-}else{
-  alert(userName + ', you need to answer yes or no...');
-}
-console.log('The response from ' + userName + ' was ' + response3);
 
-var response4 = prompt('Would I recommend this career path to everyone?').toLowerCase();
-if (response4 === answer || response4 === answer[0]) {
-  alert('That is incorrect  ' + userName + '. Even though it\'s rewarding, this is a hard path and only those truly dedicated to learning the tech will succeed!');
-}else if (response4 === wrong || response4 === wrong[0]) {
-  alert('You are correct ' + userName + '. If you have a drive to change your life for the better than this is a very rewarding career path.');
-  gameCount++;
-}else{
-  alert(userName + ', that was not the answer I was expecting...');
+function question3() {
+  response = prompt('Am I just bored doing 201 again?').toLowerCase();
+  if (response === answer || response === answer[0]) {
+    alert('Wrong answer ' + userName + ', I pick something up every time I go through a Code Fellows class.  I love that!');
+  }else if (response === wrong || response === wrong[0]) {
+    alert('You are correct ' + userName + ', I\'m having fun learning with my cohort.');
+    gameCount++;
+  }else{
+    alert(userName + ', you need to answer yes or no...');
+  }
+  console.log('The response from ' + userName + ' was ' + response);
 }
-console.log('The response from ' + userName + ' was ' + response4);
 
-var response5 = prompt('Did I chose to go into the technology industry for the money?').toLowerCase();
-if (response5 === answer || response5 === answer[0]) {
-  alert('That is incorrect  ' + userName + '. Although the money is good, my primary motivation for the career shift is so I can spend more time being present for my children as they move into adulthood.');
-}else if (response5 === wrong || response5 === wrong[0]) {
-  alert('You are correct ' + userName + '. Although the money is good, my primary motivation for the career shift is so I can spend more time being present for my children as they move into adulthood.');
-  gameCount++;
-}else{
-  alert(userName + ', if you\'re not going to give real answers var\'s move on to something else.');
+function question4() {
+  response = prompt('Would I recommend this career path to everyone?').toLowerCase();
+  if (response === answer || response === answer[0]) {
+    alert('That is incorrect  ' + userName + '. Even though it\'s rewarding, this is a hard path and only those truly dedicated to learning the tech will succeed!');
+  }else if (response === wrong || response === wrong[0]) {
+    alert('You are correct ' + userName + '. If you have a drive to change your life for the better than this is a very rewarding career path.');
+    gameCount++;
+  }else{
+    alert(userName + ', that was not the answer I was expecting...');
+  }
+  console.log('The response from ' + userName + ' was ' + response);
 }
-console.log('The response from ' + userName + ' was ' + response5);
+
+function question5() {
+  var response = prompt('Did I chose to go into the technology industry for the money?').toLowerCase();
+  if (response === answer || response === answer[0]) {
+    alert('That is incorrect  ' + userName + '. Although the money is good, my primary motivation for the career shift is so I can spend more time being present for my children as they move into adulthood.');
+  }else if (response === wrong || response === wrong[0]) {
+    alert('You are correct ' + userName + '. Although the money is good, my primary motivation for the career shift is so I can spend more time being present for my children as they move into adulthood.');
+    gameCount++;
+  }else{
+    alert(userName + ', if you\'re not going to give real answers var\'s move on to something else.');
+  }
+  console.log('The response from ' + userName + ' was ' + response);
+}
 
 var count = 1;
 var spent = 3;
@@ -137,3 +154,8 @@ if(gameCount >= 5) {
   document.write('Too bad ' + userName + ' you really need to sharpen your skills.  May I suggest some classes at Code Fellows?');  
 }
 
+question1(response);
+question2(response);
+question3(response);
+question4(response);
+question5(response);
